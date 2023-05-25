c2c75f71a747a3b934925d9a38934baadc6ea177
Your Name <shahanricky86@gmail.com>
2023-05-25 16:35:44 +0600

    our-menu-section

--- src/Pages/Home/Popular/Popular.jsx
+++ src/Pages/Home/Popular/Popular.jsx
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../component/SectionTitle/SectionTitle';
 import MenuItem from '../../Shared/MenuItem/MenuItem';
+import useMenu from '../../../hooks/useMenu';
 
 const Popular = () => {
-    const [menu,setMenu] = useState([]);
-
-    useEffect(()=>{
-        fetch("menu.json")
-        .then(res => res.json())
-        .then(data => {
-            const popularItems =data.filter(item => item.category === "popular")
-            setMenu(popularItems) 
-        })
-    }, [])
+    const [menu] = useMenu();
+    const popularItems = menu.filter(item => item.category === "popular")
+    
     return (
         <section className='mb-12'>
             <SectionTitle
-            heading={"From Our Menu"}
-            subHeading={"Popular Items"}
+                heading={"From Our Menu"}
+                subHeading={"Popular Items"}
             >
             </SectionTitle>
             <div className='grid md:grid-cols-2 gap-10'>
                 {
-                    menu.map(item => <MenuItem
-                    key={item._id}
-                    item={item}
+                    popularItems.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
                     ></MenuItem>)
                 }
+            </div>
+            <div className='text-center mt-10'>
+                <button className="btn btn-outline border-0 border-b-4
+             text-yellow-700">View Full Menu</button>
             </div>
         </section>
     );
---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../component/SectionTitle/SectionTitle';
+import MenuCategory from '../../Shared/MenuCategory/MenuCategory';
+import menuImg from "../../../assets/menu/banner3.jpg"
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg"
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg"
+import saladImg from "../../../assets/menu/salad-bg.jpg"
+import soupImg from "../../../assets/menu/soup-bg.jpg"
+
+const Menu = () => {
+    const [menu] = useMenu()
+
+    const offered = menu.filter(item => item.category === "offered")
+    const desserts = menu.filter(item => item.category === "dessert")
+    const pizzas = menu.filter(item => item.category === "pizza")
+    const salads = menu.filter(item => item.category === "salad")
+    const soups = menu.filter(item => item.category === "soup")
+
+
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boos | Menu</title>
+            </Helmet>
+            <Cover
+                img={menuImg}
+                title="Our menu"></Cover>
+            <SectionTitle
+                subHeading="Don't Miss"
+                heading="Todays Offer"
+            ></SectionTitle>
+
+            {/* Offered Section */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* Dessert Section */}
+            <MenuCategory items={desserts} title="Deserts"  img={dessertImg}></MenuCategory>
+            {/* Pizzas Section */}
+            <MenuCategory items={pizzas} title="Pizzas"  img={pizzaImg}></MenuCategory>
+            {/* Salads Section */}
+            <MenuCategory items={salads} title="Salads"  img={saladImg}></MenuCategory>
+            {/* Soups Section */}
+            <MenuCategory items={soups} title="Soups"  img={soupImg}></MenuCategory>
+            
+        </div>
+    );
+};
+
+export default Menu;
