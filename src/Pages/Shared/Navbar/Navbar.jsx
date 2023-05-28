96407a114ffd18bc153b620ccaabe8872b49ae90
Your Name <shahanricky86@gmail.com>
2023-05-28 19:54:39 +0600

    some problem fix and adding authentication

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../Providers/AuthProvider";
 
 const Navbar = () => {
+    const { user,logOut } = useContext(AuthContext);
+    const handleLogout = () =>{
+        logOut()
+        .then()
+        .catch(error =>{
+            console.log(error)
+        })
+    }
     const navItems = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Our Menu</Link></li>
         <li><Link to="/order/salad">Order Food</Link></li>
-        <li><Link to="/login">Login</Link></li>
+        {user ?
+            <>
+                <button onClick={handleLogout} className="btn btn-ghost">Log Out</button>
+            </> :
+            <>
+                <li><Link to="/login">Login</Link></li>
+            </>
+        }
     </>
     return (
         <>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,37 +1,80 @@
 import React from 'react';
+import { useContext } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useForm } from 'react-hook-form';
+import { AuthContext } from '../../Providers/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const {CreateUser} = useContext(AuthContext)
+    const { register, handleSubmit,  formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        CreateUser(data.email,data.password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
+
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">SignUp now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                        </div>
+        <>
+            <Helmet>
+                <title>Bistro Boos | Sign up</title>
+            </Helmet>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">SignUp now!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" {...register("name", { required: true })} name='name' placeholder="name" className="input input-bordered" />
+                                {errors.name && <span className='text-red-500'>Name  is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" {...register("email", { required: true })} name='email' placeholder="email" className="input input-bordered" />
+                                {errors.email && <span className='text-red-500'>Email  is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                    pattern: /^(?=.*[A-Z])(?=.*[!@#$&])(?=.*[0-9])(?=.*[a-z])/
+                                })} name='password' placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'required' && <p role="alert" className='text-red-500'> Password  required</p>}
+                                {errors.password?.type === 'minLength' && <p role="alert" className='text-red-500'> Password must be 6  character</p>}
+                                {errors.password?.type === 'maxLength' && <p role="alert" className='text-red-500'> Password must be less then 20 character</p>}
+                                {errors.password?.type === 'pattern' && <p role="alert" className='text-red-500'> Password must be one uppercase one lowercase one special character</p>}
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input type="submit" className="btn btn-primary" value="Sign Up" />
+                            </div>
+                            <p className='text-center'><small> <Link to="/login">Already have an account</Link></small></p>
+                        </form>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
